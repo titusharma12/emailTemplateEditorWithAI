4d57fea5e97b34f190ddd25f8073bae16c9d6763
sharmatitu12 <sharmatitu119@gmail.com>
2025-11-09 23:41:06 +0530

    changes

--- src/components/Data/Layout.ts
+++ src/components/Data/Layout.ts
@@ -1,29 +1,28 @@
-import { Columns2, Columns3, Columns4, RectangleHorizontal } from "lucide-react";
+import { Columns, Columns2, Columns3, Columns4, RectangleHorizontal } from "lucide-react";
 
 export default [
     {
-        label:'Column',
-        type:'column',
-        numOfCol:1,
-        icon:RectangleHorizontal,
+        label: 'Column',
+        type: 'column',
+        numOfCol: 1,
+        icon: RectangleHorizontal
     },
     {
-        label:'2 Column ',
-        type:'column-2',
-        numOfCol:2,
-        icon:Columns2,
+        label: '2 Column',
+        type: 'column',
+        numOfCol: 2,
+        icon: Columns2
     },
     {
-        label:'3 Column',
-        type:'column-3',
-        numOfCol:3,
-        icon:Columns3,
+        label: '3 Column',
+        type: 'column',
+        numOfCol: 3,
+        icon: Columns3
     },
     {
-        label:'4 Column ',
-        type:'column-4',
-        numOfCol:4,
-        icon:Columns4,
-    },
-    
+        label: '4 Column',
+        type: 'column',
+        numOfCol: 4,
+        icon: Columns4
+    }
 ]
